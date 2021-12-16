fc3af4b1ec5ef33b11fd1efacfcc36b0a713fefb
houseofsam <samxsaid@gmail.com>
2021-12-15 20:47:44 -0500

    create loadTweets function to asynchronously fetch tweets data. loadTweets is immediately invoked upon page load and invoked in the success callback function upon new tweet submission. For now the new tweet gets appended to the bottom of the tweet-container class after clearing it to prevent duplicates, but this will have to be changed in the future so that it gets appended to the top of the container instead (and perhaps with a better solution to preventing duplicates)

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,44 +4,24 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const data = [
-  {
-    "user": {
-      "name": "Walter White",
-      "avatars": "<i class=\"fas fa-user-secret fa-2x\"></i>"
-      ,
-      "handle": "@Heisenberg"
-    },
-    "content": {
-      "text": "I am the one who knocks"
-    },
-    "created_at": Date.now()
-  },
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
-
 $(document).ready(function() {
+  // Function to asynchronously fetch tweet data from '/tweets' via AJAX GET
+  // If successful, it will call the renderTweets function above and pass it the JSON response data that is returned from the request as an argument
+  // This function will also be invoked after every successful POST request when a tweet is submitted
+  const loadTweets = function() {
+    $.ajax({
+      url: '/tweets',
+      type: 'GET',
+    })
+    .done((response) => {
+      renderTweets(response);
+    })
+    .fail((error) => {
+      console.log('error:', error)
+    })
+  }
+
+  loadTweets();
 
   // Checks whether the avatars property contains an http link or an svg class from one of the project libraries
   const avatarType = function(avatar) {
@@ -97,13 +77,10 @@ $(document).ready(function() {
     }
   };
 
-  renderTweets(data);
-
   // prevent page from navigating upon form submission
   $('.new-tweet form').on('submit', function(e) {
     e.preventDefault();
     const $formData = $(this).serialize();
-    console.log($formData);
     
     // asynchronously send the data to server
     $.ajax({
@@ -111,14 +88,18 @@ $(document).ready(function() {
       type: 'POST',
       data: $formData
     })
-    .done((response) => {
-      console.log('response:', response);
-      console.log('data:', data);
+    .done(() => {
+      // clear new tweet input field
+      $(this).children('#tweet-text').val('');
+      $(this).parent().siblings('#tweets-container').html('');
+      // fetch tweets from DB and render them to page
+      loadTweets();
     })
     .fail((error) => {
       console.log('error:', error)
     })
   });
+  
   
 
 });
